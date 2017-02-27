1008627c26c10fd0991353f4001104879ab438c8
Emily Mikl <emilysmikl@gmail.com>
2017-02-27 16:08:28 -0600

    Initial commit for Angular church website

---
+++ README.md
@@ -0,0 +1,4 @@
+# Angular2Bones
+1. Clone into local workspace
+2. Run npm install
+3. To start server, run npm start
---
+++ systemjs.config.js
@@ -0,0 +1,45 @@
+(function(global) {
+    var map = {
+            'app': 'dist',
+            '@angular': 'node_modules/@angular',
+            'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+            'rxjs': 'node_modules/rxjs'
+        },
+        packages = {
+            'app': { main: 'main.js',  defaultExtension: 'js' },
+            'angular2-in-memory-web-api': {main: 'index.js', defaultExtension:'js'},
+            'rxjs': { defaultExtension: 'js' }
+        },
+        ngPackageNames = [
+            'common',
+            'compiler',
+            'core',
+            'forms',
+            'http',
+            'platform-browser',
+            'platform-browser-dynamic',
+            'router',
+            'router-deprecated',
+            'upgrade'
+        ];
+
+    function packIndex(pkgName) {
+        packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    }
+
+    function packUmd(pkgName) {
+        packages['@angular/'+pkgName] = {main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js'};
+    }
+
+    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
+    ngPackageNames.forEach(setPackageConfig);
+
+    var config = {
+        map: map,
+        packages: packages
+    };
+
+    System.config(config);
+
+})(this);
